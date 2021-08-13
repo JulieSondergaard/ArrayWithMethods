bbb63b12eae4435fa394b18feda2383a7e8776b7
JulieSøndergaard-Hilleke <jsh@itklyngen.dk>
2021-08-13 10:50:13 +0200

    Add project files.

---
+++ ArrayWithMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayWithMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetNumbers();
+        }
+        public static void GetNumbers()
+        {
+            int[] numbers = new int[9];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+
+                if (i == 5)
+                {
+                    numbers[i] = numbers[i - 1] * 2;
+                }
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
